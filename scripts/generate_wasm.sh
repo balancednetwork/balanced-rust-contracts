d581cfd7d8c87fd5650986020783067203a65a6c
Night Owl <nightowl121@protonmail.com>
2023-06-06 11:06:26 +0545

    feat: use 1.69.0 rust version
    * install wasm-opt and cw-check in ci

--- scripts/generate_wasm.sh
+++ scripts/generate_wasm.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 set -e
 
-# Download wasm-opt
-BINARYEN_VERS=110
-BINARYEN_DWN="https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERS}/binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz"
-if ! which wasm-opt; then
-  curl -OL $BINARYEN_DWN
-  tar xf binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz
-  export PATH=$PATH:$PWD/binaryen-version_${BINARYEN_VERS}/bin
-fi
-
 # Generate optimized wasm files and verify generated wasm with cosmwasm-check
 mkdir -p artifacts
 cargo wasm
-cargo install cosmwasm-check
 for WASM in ./target/wasm32-unknown-unknown/release/*.wasm; do
   NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
   echo "########Creating intermediate hash for $NAME ...########"
