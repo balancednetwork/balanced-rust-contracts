8aeb852e112c01a225d61e03a3f9c2b395ac93da
BDEVcoder <ujwal.bdev@gmail.com>
2023-06-11 10:53:28 +0545

    feat: Add XcallMsg related Data Structures

--- contracts/cw-common/src/AssetManager_msg.rs
+++ contracts/cw-common/src/AssetManager_msg.rs
@@ -1,17 +1,8 @@
 use cosmwasm_schema::cw_serde;
 
 
-//External Methods
-#[cw_serde]
-pub struct  Deposit {
-pub token_address: String,
-pub from_address: String,
-pub to_address: String,
-pub amount: u128,
-pub data : Vec<u8>,
-}
 
-
+//structs to create XCALL messages
 #[cw_serde]
 pub struct WithdrawRequest {
     token_address: String,
@@ -19,11 +10,8 @@ pub struct WithdrawRequest {
         amount: u128,
 } 
 
-
-
-//Xcall Methods
 #[cw_serde]
-pub struct  DepositTo {
+pub struct  Deposit {
 pub token_address: String,
 pub from_address: String,
 pub to_address : String,
@@ -31,25 +19,32 @@ pub amount: u128,
 pub data: Vec<u8>,
 }
 
-
 pub struct DepositRevert {
     pub caller: String,
     pub amount: Uint128,
 }
 
-
 pub struct WithdrawTo {
-    token_address: String,
-        user_address:  String,
-        amount: u128,
+    pub token_address: String,
+    pub user_address:  String,
+    pub amount: u128,
 }
 
 
 
 #[cw_serde]
 pub enum ExecuteMsg {
-   deposit(Deposit),
-   withdraw_request(WithdrawRequest),
+   deposit {
+    pub from_address: String,
+    pub to_address: String,
+    pub amount: u128,
+   },
+
+   withdraw_request{
+    token_address: String,
+        from_address:  String,
+        amount: u128,
+   },
 }
 
 
@@ -58,13 +53,11 @@ pub enum XcallMsg {
     HandlecallMsg {
         deposit_revert : DepositRevert,
         withdraw_to : WithdrawTo,
-
     },
     
     SendcallMsg {
         deposit_from : Deposit,
-        withdraw_req_from : WithdrawRequest,
-        
+        withdraw_req_from : WithdrawRequest,   
     }
 }
 
