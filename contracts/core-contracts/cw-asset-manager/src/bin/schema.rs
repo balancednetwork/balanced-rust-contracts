ac98f66e30e3b252c754a70e0efac7eb5473ed86
Night Owl <nightowl121@protonmail.com>
2023-06-12 15:34:44 +0545

    feat: use execute msg from common library

--- contracts/core-contracts/cw-asset-manager/src/bin/schema.rs
+++ contracts/core-contracts/cw-asset-manager/src/bin/schema.rs
@@ -1,6 +1,7 @@
 use cosmwasm_schema::write_api;
 
-use cw_asset_manager::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_asset_manager::msg::{InstantiateMsg, QueryMsg};
+use cw_common::asset_manager_msg::ExecuteMsg;
 
 fn main() {
     write_api! {
