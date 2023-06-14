99732a65409a1e336cee7a6678c9f44adfb2937b
BDEVcoder <ujwal.bdev@gmail.com>
2023-06-14 13:54:10 +0545

    feat: Replace type for the amount field

--- contracts/cw-common/src/asset_manager_msg.rs
+++ contracts/cw-common/src/asset_manager_msg.rs
@@ -1,11 +1,12 @@
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Uint128;
 
 //structs to create XCALL messages
 #[cw_serde]
 pub struct WithdrawRequest {
     pub token_address: String,
     pub from_address: String,
-    pub amount: u128,
+    pub amount: Uint128,
 }
 
 #[cw_serde]
@@ -13,29 +14,29 @@ pub struct Deposit {
     pub token_address: String,
     pub from_address: String,
     pub to_address: String,
-    pub amount: u128,
+    pub amount: Uint128,
     pub data: Vec<u8>,
 }
 
 #[cw_serde]
 pub struct DepositRevert {
     pub caller: String,
-    pub amount: u128,
+    pub amount: Uint128,
 }
 
 #[cw_serde]
 pub struct WithdrawTo {
     pub token_address: String,
     pub user_address: String,
-    pub amount: u128,
+    pub amount: Uint128,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
     //executor address can be extracted at processing side
-    Deposit { token_address: String, amount: u128 },
+    Deposit { token_address: String, amount: Uint128 },
 
-    WithdrawRequest { token_address: String, amount: u128 },
+    WithdrawRequest { token_address: String, amount: Uint128 },
 }
 
 #[cw_serde]
