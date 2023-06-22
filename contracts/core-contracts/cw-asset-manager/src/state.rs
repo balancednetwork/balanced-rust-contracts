9cb46497c7b76d3f14f5808e39ee7f3b3cbcfe39
BDEVcoder <ujwal.bdev@gmail.com>
2023-06-22 12:45:44 +0545

    feat: add states for xcall integration

--- contracts/core-contracts/cw-asset-manager/src/state.rs
+++ contracts/core-contracts/cw-asset-manager/src/state.rs
@@ -15,3 +15,13 @@ pub const USER_TOKENS: Map<&Addr,Vec<Addr>> = Map::new("user_tokens");
 
 
 
+
+
+pub const X_CALL: Item<String> = Item::new("xCall");
+pub const X_CALL_BTP_ADDRESS: Item<String> = Item::new("xCallBTPAddress");
+pub const NID: Item<String> = Item::new("nid");
+pub const HUB_ADDRESS: Item<String> = Item::new("hubAddress");
+pub const HUB_NET: Item<String> = Item::new("hubNet");
+
+
+
