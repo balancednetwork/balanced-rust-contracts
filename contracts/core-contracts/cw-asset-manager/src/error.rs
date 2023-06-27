e3f39fa8bfd9ee9dbb02699ed783273ec0b7c344
BDEVcoder <ujwal.bdev@gmail.com>
2023-06-27 14:18:48 +0545

    feat: Add variant rlp error variat fro contractError

--- contracts/core-contracts/cw-asset-manager/src/error.rs
+++ contracts/core-contracts/cw-asset-manager/src/error.rs
@@ -1,23 +1,42 @@
 use cosmwasm_std::{StdError, Addr};
 use thiserror::Error;
+use rlp::DecoderError;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug,PartialEq)]
 pub enum ContractError {
     //If a StdError is encountered and returned, it will be automatically converted into a ContractError using the #[from] attribute
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("RLP decoding error: {0}")]
+    RlpDecodingError(#[from] DecoderError),
 
     #[error("Unacceptable token address: {address}")]
     InvalidToken {
         address: Addr,
     },
 
-
-    #[error("Unable to parse data from response")]
-    ErrorInParsing{},
-
     
 
+    #[error("Subcall failed: {error}")]
+    SubcallFailed{
+        error: String,
+    },
 
+    #[error("Token Transfer Failed : {reason}")]
+    TokenTransferFailure {
+        reason: String,
+    },
+
+    #[error("Xcall BTP Address is not found")]
+    AddressNotFound,
+    
+    #[error("unknown method extracted while decoding rlp bytes")]
+    UnknownMethod,
+
+   
     
 }
+
+
+
