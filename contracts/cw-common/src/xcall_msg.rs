86ffda35bc9e343cd56cbab5a8f3fd668eeaadc0
nova032 <ujjwalshrestha42@gmail.com>
2023-07-27 14:00:48 +0545

    feat(xcall_msg): added necessary variants from cw-hub-bnUSD

--- contracts/cw-common/src/xcall_msg.rs
+++ contracts/cw-common/src/xcall_msg.rs
@@ -1,4 +1,9 @@
+use crate::network_address::NetId;
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Addr;
+
+#[cw_serde]
+pub struct InstantiateMsg {}
 
 #[cw_serde]
 struct NetworkAddress {
@@ -23,9 +28,8 @@ pub enum XCallQuery {
 pub enum XCallMsg {
     // @param data represent hex encoded string / hex encoded number in vec
     // @param to represent the callee contract which recieve the data on the destination chain
-    // @param rollback is for handling error cases. 
+    // @param rollback is for handling error cases.
     SendCallMessage {
-
         //  The BTP address of the callee on  the destination chain
         to: String,
 
@@ -34,7 +38,32 @@ pub enum XCallMsg {
 
         // (optional) The data for restoring the caller state when an error occured
         rollback: Option<Vec<u8>>,
-
         // used for returning the serial number of the request
+    },
+
+    SetDefaultConnection {
+        nid: NetId,
+        address: Addr,
+    },
+
+    HandleMessage {
+        from: NetId,
+        sn: Option<i64>,
+        msg: Vec<u8>,
+    },
+
+    ExecuteCall {
+        request_id: u128,
+        data: Vec<u8>,
+    },
+
+    ExecuteRollback {
+        sequence_no: u128,
+    },
+
+    TestHandleCallMessage {
+        from: String,
+        data: Vec<u8>,
+        cw20_token: String,
     },
 }
