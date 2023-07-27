c6309a35e1c72d94a41d908b4ed9faad530c9217
nova032 <ujjwalshrestha42@gmail.com>
2023-07-27 13:57:21 +0545

    chore: Remove unecessary tests from integration_test.rs

--- contracts/core-contracts/cw-asset-manager/src/integration_test.rs
+++ contracts/core-contracts/cw-asset-manager/src/integration_test.rs
@@ -1,15 +1,8 @@
-use cosmwasm_std::testing::MOCK_CONTRACT_ADDR;
-use cosmwasm_std::{coins, to_binary, Addr, Attribute, Empty, QuerierWrapper, WasmMsg, Uint128,ContractResult, ContractInfoResponse, SystemResult,WasmQuery};
-use cw20::{Cw20Coin, Cw20Contract, Cw20ExecuteMsg};
-use cw_multi_test::{App, AppResponse, Contract, ContractWrapper, Executor};
+use cosmwasm_std::{Addr, Empty, Uint128};
+use cw20::Cw20Coin;
+use cw_multi_test::{App, Contract, ContractWrapper, Executor};
 
 use cw_common::asset_manager_msg::*;
-
-use cw_common::xcall_data_types::{Deposit, DepositRevert};
-use cw_common::xcall_msg::{XCallMsg, XCallQuery};
-use rlp::Encodable;
-
-use crate::error::ContractError;
 
 const OWNER: &str = "owner";
 
@@ -31,18 +24,15 @@ pub fn contract_cw20() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
-
 fn mock_app() -> App {
     App::default()
 }
-
 
 /* //////////////////////////////////////////////////
             CHECK cw20 instantiate
 /////////////////////////////////////////////////// */
 
 #[test]
-
 
 fn test_cw20_instantiate() {
     let mut app = mock_app();
@@ -61,7 +51,6 @@ fn test_cw20_instantiate() {
     };
     let resp = app.instantiate_contract(cw20_id, owner.clone(), &msg, &[], "SPOKE", None);
     assert!(resp.is_ok());
-    
 }
 
 /* //////////////////////////////////////////////////
@@ -75,11 +64,10 @@ fn test_asset_manager_instantiation() {
     let asset_id = app.store_code(contract_assetmanager());
     let msg = InstantiateMsg {};
 
-    let asst_addr = app
-        .instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None);
+    let asst_addr =
+        app.instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None);
     assert!(asst_addr.is_ok());
 }
-
 
 /* //////////////////////////////////////////////////
             testing the cw20 deposit
@@ -89,114 +77,111 @@ fn test_asset_manager_instantiation() {
 fn test_deposit() {
     let mut app = mock_app();
 
-   // assigned  owner
-   let owner = Addr::unchecked("owner");
-   let cw20_id = app.store_code(contract_cw20());
-   let init_msg = cw20_base::msg::InstantiateMsg {
-       name: "Spokey".to_string(),
-       symbol: "SPOK".to_string(),
-       decimals: 18,
-       initial_balances: vec![Cw20Coin {
-           address: owner.clone().to_string(),
-           amount: Uint128::new(5000),
-       }],
-       mint: None,
-       marketing: None,
-   };
-   let resp = app
-       .instantiate_contract(cw20_id, owner.clone(), &init_msg, &[], "SPOKE", None)
-       .unwrap();
+    // assigned  owner
+    let owner = Addr::unchecked("owner");
+    let cw20_id = app.store_code(contract_cw20());
+    let init_msg = cw20_base::msg::InstantiateMsg {
+        name: "Spokey".to_string(),
+        symbol: "SPOK".to_string(),
+        decimals: 18,
+        initial_balances: vec![Cw20Coin {
+            address: owner.clone().to_string(),
+            amount: Uint128::new(5000),
+        }],
+        mint: None,
+        marketing: None,
+    };
+    let resp = app
+        .instantiate_contract(cw20_id, owner.clone(), &init_msg, &[], "SPOKE", None)
+        .unwrap();
 
-   let asset_id = app.store_code(contract_assetmanager());
+    let asset_id = app.store_code(contract_assetmanager());
 
-   let msg = InstantiateMsg {};
+    let msg = InstantiateMsg {};
 
-   let asset_addr = app
-       .instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None)
-       .unwrap();
-   let msg = ExecuteMsg::Deposit {
-       token_address: resp.clone().to_string(),
-       amount: Uint128::new(1500128),
-       to: Some(String::from(
-           "0x01.icon/cx9876543210fedcba9876543210fedcba98765432",
-       )),
-       data: None,
-   };
+    let asset_addr = app
+        .instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None)
+        .unwrap();
+    let msg = ExecuteMsg::Deposit {
+        token_address: resp.clone().to_string(),
+        amount: Uint128::new(1500128),
+        to: Some(String::from(
+            "0x01.icon/cx9876543210fedcba9876543210fedcba98765432",
+        )),
+        data: None,
+    };
 
-   let _exec_resp = app.execute_contract(owner.clone(), asset_addr.clone(), &msg, &[]);
+    let _exec_resp = app.execute_contract(owner.clone(), asset_addr.clone(), &msg, &[]);
 
-   let msg = cw20::Cw20ExecuteMsg::Transfer {
-        
+    let msg = cw20::Cw20ExecuteMsg::Transfer {
         recipient: String::from("0x01.icon/cx9876543210fedcba9876543210fedcba98765432"),
         amount: Uint128::new(100),
-   };
+    };
 
-   let exec_deposit = app.execute_contract(owner.clone(), resp.clone(), &msg, &[]);
+    let exec_deposit = app.execute_contract(owner.clone(), resp.clone(), &msg, &[]);
 
-   //     for attribute in &exec_deposit.events[0].attributes {
-   //         match attribute {
-   //             Attribute { key, value } => match key.as_str() {
-   //                 "Token" => assert_eq!(value, "Spokey"),
-   //                 "To" => println!("value: {:?}",value),
-   //                 "Amount" => assert_eq!(value, "1500"),
-   //                 _ => panic!("Unexpected attribute key"),
-   //             },
-   //     }
-   // }
+    //     for attribute in &exec_deposit.events[0].attributes {
+    //         match attribute {
+    //             Attribute { key, value } => match key.as_str() {
+    //                 "Token" => assert_eq!(value, "Spokey"),
+    //                 "To" => println!("value: {:?}",value),
+    //                 "Amount" => assert_eq!(value, "1500"),
+    //                 _ => panic!("Unexpected attribute key"),
+    //             },
+    //     }
+    // }
 
-   assert!(exec_deposit.is_ok());
+    assert!(exec_deposit.is_ok());
 }
 
 #[test]
 
 fn test_deposit_cw20_tokens_with_zero_amount() {
-     let mut app = mock_app();
+    let mut app = mock_app();
 
-   // assigned  owner
-   let owner = Addr::unchecked("owner");
-   let cw20_id = app.store_code(contract_cw20());
-   let init_msg = cw20_base::msg::InstantiateMsg {
-       name: "Spokey".to_string(),
-       symbol: "SPOK".to_string(),
-       decimals: 18,
-       initial_balances: vec![Cw20Coin {
-           address: owner.clone().to_string(),
-           amount: Uint128::zero(),
-       }],
-       mint: None,
-       marketing: None,
-   };
-   let resp = app
-       .instantiate_contract(cw20_id, owner.clone(), &init_msg, &[], "SPOKE", None)
-       .unwrap();
+    // assigned  owner
+    let owner = Addr::unchecked("owner");
+    let cw20_id = app.store_code(contract_cw20());
+    let init_msg = cw20_base::msg::InstantiateMsg {
+        name: "Spokey".to_string(),
+        symbol: "SPOK".to_string(),
+        decimals: 18,
+        initial_balances: vec![Cw20Coin {
+            address: owner.clone().to_string(),
+            amount: Uint128::zero(),
+        }],
+        mint: None,
+        marketing: None,
+    };
+    let resp = app
+        .instantiate_contract(cw20_id, owner.clone(), &init_msg, &[], "SPOKE", None)
+        .unwrap();
 
-   let asset_id = app.store_code(contract_assetmanager());
+    let asset_id = app.store_code(contract_assetmanager());
 
-   let msg = InstantiateMsg {};
+    let msg = InstantiateMsg {};
 
-   let asset_addr = app
-       .instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None)
-       .unwrap();
-   let msg = ExecuteMsg::Deposit {
-       token_address: resp.clone().to_string(),
-       amount: Uint128::zero(),
-       to: Some(String::from(
-           "0x01.icon/cx9876543210fedcba9876543210fedcba98765432",
-       )),
-       data: None,
-   };
+    let asset_addr = app
+        .instantiate_contract(asset_id, owner.clone(), &msg, &[], "asset_manager", None)
+        .unwrap();
+    let msg = ExecuteMsg::Deposit {
+        token_address: resp.clone().to_string(),
+        amount: Uint128::zero(),
+        to: Some(String::from(
+            "0x01.icon/cx9876543210fedcba9876543210fedcba98765432",
+        )),
+        data: None,
+    };
 
-   let _exec_resp = app.execute_contract(owner.clone(), asset_addr.clone(), &msg, &[]);
-  
+    let _exec_resp = app.execute_contract(owner.clone(), asset_addr.clone(), &msg, &[]);
 
-   let msg = cw20::Cw20ExecuteMsg::TransferFrom {
+    let msg = cw20::Cw20ExecuteMsg::TransferFrom {
         owner: owner.clone().to_string(),
         recipient: String::from("0x01.icon/cx9876543210fedcba9876543210fedcba98765432"),
         amount: Uint128::zero(),
-   };
+    };
 
-   let exec_deposit = app.execute_contract(owner.clone(), resp.clone(), &msg, &[]);
+    let exec_deposit = app.execute_contract(owner.clone(), resp.clone(), &msg, &[]);
 
-   assert!(exec_deposit.is_err());
+    assert!(exec_deposit.is_err());
 }
-
