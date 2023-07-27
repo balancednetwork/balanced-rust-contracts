8c80361f6a6451a227f044a647b01bb19b509bd8
BDEVcoder <ujwal.bdev@gmail.com>
2023-07-27 16:14:37 +0545

    feat: add tests for handle msg

---
+++ contracts/core-contracts/cw-asset-manager/tests/x_handle_msg_test.rs
@@ -0,0 +1,103 @@
+mod setup;
+
+use cosmwasm_std::Addr;
+use cw_common::{
+    network_address::{NetId, NetworkAddress},
+    x_call_msg::XCallExecuteMsg,
+    xcall_data_types::WithdrawTo,
+};
+use cw_multi_test::Executor;
+use rlp::{Encodable, RlpStream};
+use setup::{
+    execute_config_x_call, get_event, instantiate_contracts, set_default_connection, setup_context,
+    TestContext,
+};
+
+fn execute_handle_msg_on_asset_manager_from_relayer(mut ctx: TestContext) -> TestContext {
+    let token_addr = ctx.get_cw20token_app().to_string();
+    let relay = Addr::unchecked("relayer");
+
+    // ----------------------------        execution flow from RELAYER------>  XCALL       --------------------------------------------
+
+    //pretend relayer for the connection such that relay can call ExecuteCall msg on xcall
+    ctx = set_default_connection(ctx, relay.clone());
+
+    let call_data = WithdrawTo {
+        token_address: token_addr.to_string(),
+        user_address: "sender".to_string(),
+        amount: 100,
+    };
+
+    let data = call_data.rlp_bytes();
+
+    //random seq_no. for test purpose
+    let sn: u64 = 100;
+    let msg_typ: u64 = 1;
+    let from = NetworkAddress("0x01.icon/cx7866543210fedcba9876543210fedcba987654df".to_owned());
+
+    //construct encoded CallServiceMessageRequest
+    let mut stream = RlpStream::new();
+    stream.begin_list(6);
+    stream.append(&from.to_string());
+    stream.append(&token_addr);
+    stream.append(&sn);
+    stream.append(&false);
+    stream.append(&data);
+
+    let encoded_data = stream.out().to_vec();
+
+    let mut stream = RlpStream::new();
+    stream.begin_list(2);
+    stream.append(&msg_typ);
+    stream.append(&encoded_data);
+
+    let msg_data = stream.out().to_vec();
+
+    let response = ctx.app.execute_contract(
+        relay,
+        ctx.get_xcall_app(),
+        &XCallExecuteMsg::HandleMessage {
+            from: NetId::from("0x01.icon".to_owned()).to_string(),
+            sn: None,
+            msg: msg_data,
+        },
+        &[],
+    );
+
+    println!("rsp from r->xcll {:?}", response);
+
+    let event = get_event(&response.unwrap(), "wasm-CallMessage").unwrap();
+    // *`request_id`: `request_id` is a unique identifier for a specific request made by a user. It is
+    // used to retrieve the details of the request from the contract's storage and execute the
+    // corresponding action.
+    let value = event.get("reqId").unwrap();
+    let req_id = value.parse::<u128>().unwrap();
+
+    // ----------------------------        execution flow from XCALL------> ASSET MANAGER       --------------------------------------------
+
+    let response = ctx
+        .app
+        .execute_contract(
+            ctx.get_xcall_app(),
+            ctx.get_assetmanager_app(),
+            &XCallExecuteMsg::ExecuteCall {
+                request_id: req_id,
+                data: data.rlp_bytes().to_vec(),
+            },
+            &[],
+        )
+        .unwrap();
+
+    println!("withdraw Resp: {:?}", response);
+
+    ctx
+}
+
+#[test]
+fn handle_call_message_test() {
+    let mut context = setup_context();
+    context = instantiate_contracts(context);
+    let source_x_call = context.get_xcall_app();
+    context = execute_config_x_call(context, source_x_call);
+    execute_handle_msg_on_asset_manager_from_relayer(context);
+}
